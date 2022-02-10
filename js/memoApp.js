993102fc051fc1e2c7b29bb58ce020f748c73830
Ridwan Oyedele <realsure10@gmail.com>
2022-02-10 09:10:42 +0100

    added menu active class toggle function

--- js/memoApp.js
+++ js/memoApp.js
@@ -54,3 +54,38 @@ addForm.addEventListener('submit', (e) => {
 
     addForm.reset();
 })
+
+// making nav-menu actively
+// const navMenu = document.querySelector('#nav-panel .nav-menu');
+// const panels = document.querySelectorAll('.panel');
+
+// navMenu.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if (e.target.tagName == 'LI') {
+//         const targetPanel = document.querySelector(e.target.dataset.target);
+//         Array.from(panels).forEach((panel) => {
+//             if (panel == targetPanel) {
+//                 panel.classList.add('active');
+//             } else {
+//                 panel.classList.remove('active')
+//             }
+//         })
+//     }
+// })
+
+const tabs = document.querySelector('.nav-menu');
+const panels = document.querySelectorAll(".panel");
+
+tabs.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.tagName == "LI") {
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach((panel) => {
+            if (panel == targetPanel) {
+                panel.classList.add('active');
+            } else {
+                panel.classList.remove('active');
+            }
+        })
+    }
+})
